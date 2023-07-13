6a39af1ec971a8ed6114b091ab2ffe1727c79702
Pawel Wiszenko <16039496+pawello2222@users.noreply.github.com>
2023-07-14 01:39:11 +0200

    Updated Package

--- Package.swift
+++ Package.swift
@@ -1,23 +1,27 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.5
 
 import PackageDescription
 
 let package = Package(
     name: "Appliable",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Appliable",
-            targets: ["Appliable"]),
+            targets: ["Appliable"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Appliable"),
         .testTarget(
             name: "AppliableTests",
-            dependencies: ["Appliable"]),
+            dependencies: ["Appliable"]
+        ),
     ]
 )
